929101cbe6c666e9194f2775ca255f9d960c233f
nicolle <antdev1987@gmail.com>
2023-08-05 19:37:42 -0600

    anoter fix canvas

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 
+
 const nextConfig = {
   reactStrictMode: false,
 
@@ -13,17 +14,6 @@ const nextConfig = {
     domains: ['res.cloudinary.com'],
   },
 
-}
-
-if (
-  process.env.LD_LIBRARY_PATH == null ||
-  !process.env.LD_LIBRARY_PATH.includes(
-    `${process.env.PWD}/node_modules/canvas/build/Release:`,
-  )
-) {
-  process.env.LD_LIBRARY_PATH = `${
-    process.env.PWD
-  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
 }
 
 
